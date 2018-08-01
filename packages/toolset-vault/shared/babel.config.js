3ee9ec46467c95d5fb4dc2640348e3f7ae1cdc32
iridian <iridian@iki.fi>
2018-08-01 04:47:00 +0300

    Removes babel/preset-stage-0 in favor of explicit proposal deps

--- packages/toolset-vault/shared/babel.config.js
+++ packages/toolset-vault/shared/babel.config.js
@@ -8,27 +8,77 @@ module.exports = function configureBabel (api, rootPrefix) {
     presets: [
       "@babel/preset-env",
       "@babel/preset-react",
-      ["@babel/preset-stage-0", { decoratorsLegacy: true, pipelineProposal: "minimal" }],
     ],
-    plugins: [],
-  };
-  const plugins = {
-    "@babel/plugin-transform-runtime": {
-      // There is a bug with babel 7.0.0-beta.49: https://github.com/babel/babel/issues/8061
-      helpers: false,
-      // regenerator:true would remove global.regeneratorRuntime which some libraries need
-      // (notably: indexeddbshim)
-      regenerator: false,
-    },
-    "babel-plugin-root-import": { rootPathSuffix: "packages" },
-    "@babel/plugin-proposal-decorators": { legacy: true },
-    "@babel/plugin-transform-flow-strip-types": undefined,
+    plugins: [
+      ["@babel/plugin-transform-runtime", {
+        // There is a bug with babel 7.0.0-beta.49: https://github.com/babel/babel/issues/8061
+        helpers: false,
+        // regenerator:true would remove global.regeneratorRuntime which some libraries need
+        // (notably: indexeddbshim)
+        regenerator: false,
+      }],
+      ["babel-plugin-root-import", { rootPathSuffix: "packages" }],
+
+      // This MUST come before proposal-class-properties. Otherwise places which use hasOwnProperty
+      // to test for presence of cached values will break: class-properties will interpret flow
+      // type as instruction to initialize variables to undefined.
+      "@babel/plugin-transform-flow-strip-types",
+
+      // Stage 0
+
+      // "@babel/plugin-proposal-function-bind",
+
+      // Stage 1
+
+      // export ns from / export default from
+      "@babel/plugin-proposal-export-default-from",
+
+      // foo ||= bar ~== (foo = (foo || bar))
+      // "@babel/plugin-proposal-logical-assignment-operators",
+
+      // foo?.bar ~== (foo || {}).bar - This would be soo useful but isn't used atm.
+      // ["@babel/plugin-proposal-optional-chaining", { loose: false }],
+
+      // value |> unaryCallback |> anotherCallback ~== anotherCallback(unaryCallback(value))
+      // ["@babel/plugin-proposal-pipeline-operator", { proposal: "minimal" }],
+
+      // foo ?? "if-nullish" ~== (foo != null) ? foo : "if-nullish"
+      // ["@babel/plugin-proposal-nullish-coalescing-operator", { loose: false }],
+
+      // do { if (x > 10) "big" else "small" } ~== (x > 10 ? "big" : "small")
+      // "@babel/plugin-proposal-do-expressions",
+
+      // Stage 2
+
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+
+      // Generator sent syntax
+      // "@babel/plugin-proposal-function-sent",
+
+      // export * as ns from
+      // "@babel/plugin-proposal-export-namespace-from",
+
+      // 1_000_000
+      // "@babel/plugin-proposal-numeric-separator",
+
+      // foo || (throw new Error ("falsy!"))
+      // "@babel/plugin-proposal-throw-expressions",
+
+      // Stage 3
+
+      // import()
+      // "@babel/plugin-syntax-dynamic-import",
+
+      // "@babel/plugin-syntax-import-meta",
+      ["@babel/plugin-proposal-class-properties", { loose: false }],
+
+      // line separators etc. inside regular "" strings
+      // "@babel/plugin-proposal-json-strings"
+    ],
   };
   if (process.env.TARGET_ENV === "package-assemble") {
-    delete plugins["babel-plugin-root-import"];
-    plugins["module-resolver"] = { root: ["./packages"], alias: { "~": rootPrefix } };
+    ret.plugins = ret.plugins.filter(plugin => (plugin[0] !== "babel-plugin-root-import"));
+    ret.plugins.push(["module-resolver", { root: ["./packages"], alias: { "~": rootPrefix } }]);
   }
-  Object.keys(plugins).forEach(key =>
-    ret.plugins.push(plugins[key] === undefined ? key : [key, plugins[key]]));
   return ret;
 };

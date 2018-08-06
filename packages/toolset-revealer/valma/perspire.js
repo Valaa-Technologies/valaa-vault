24a71a392a633c88bc1413b7fe979062f16bea6a
Ismo Torvinen <ismo.torvinen@gmail.com>
2018-08-06 17:35:47 +0300

    nodejs support for Revelation.js
    modified trySpread function to use `require()` instead of `request()`.
    modified perspire.js to use require(json) instead of using `fs`.

--- packages/toolset-revealer/valma/perspire.js
+++ packages/toolset-revealer/valma/perspire.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env vlm
 
 global.window = global;
-const fs = require("fs");
+const path = require("path");
 const inspire = require("@valos/inspire");
-const deepExtend = require("@valos/tools/deepExtend").default;
 
 exports.command = "perspire [revelationPath]";
 exports.describe = "headless server-side environment";
@@ -16,24 +15,14 @@ exports.handler = (yargv) => {
   // Only enabled inside package
   const vlm = yargv.vlm;
 
-  let revelationPath = yargv.revelationPath || "./valaa.json";
-//  if (revelationPath.search("valaa.json") === -1) {
-//    revelationPath += "valaa.json";
-//  }
-//  console.log("revelationPath", revelationPath);
+  const revelationPath = yargv.revelationPath || "./valaa.json";
 
   if (!vlm.shell.test("-f", revelationPath)) {
     vlm.info(`file not found ${revelationPath}`);
     process.exit();
   } else {
-    const revelation = JSON.parse(fs.readFileSync(revelationPath));
+    const revelation = require(path.join(process.cwd(), revelationPath));
+    global.revelationPath = path.dirname(revelationPath);
     const perspire = inspire.default(revelation);
   }
 };
-
-/*
-fs.readFile(revelationPath, (err, data) => {
-  // perspire commands here!
-  const dataJson = JSON.parse(data);
-}
-*/

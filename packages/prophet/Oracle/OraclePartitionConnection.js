a999d6673be1d8afdd83811556ddcb61201c64be
iridian <iridian@iki.fi>
2018-10-04 09:42:34 +0300

    refactor(prophet): Refactor Oracle
    Adds legacy-event conversion responsibilities (not implemented yet).
    Moves authority partition connections to be directly upstream of
      each OraclePartitionConnection.
    Removes downstream pending command queues (will add to FalseProphet).
    Removes most of the narrate/chronicle media logic (will add to Scribe).
    Retains collection request batching.
    Doesn't fix tests yet.

--- packages/prophet/Oracle/OraclePartitionConnection.js
+++ packages/prophet/Oracle/OraclePartitionConnection.js
@@ -1,16 +1,16 @@
 // @flow
 
-import type Command, { UniversalEvent } from "~/raem/command";
+import type { UniversalEvent } from "~/raem/command";
+import { createValaaURI } from "~/raem/ValaaURI";
 
 import PartitionConnection from "~/prophet/api/PartitionConnection";
-import type { ChronicleOptions, ConnectOptions, NarrateOptions, MediaInfo, RetrieveMediaContent }
+import type { ChronicleOptions, ConnectOptions, NarrateOptions, MediaInfo }
     from "~/prophet/api/Prophet";
 
-import { dumpObject, invariantifyObject } from "~/tools";
+import { dumpObject } from "~/tools";
 
-import { _connect, _chronicleEventLog, _narrateEventLog } from "./_connectionOps";
-import { _createReceiveTruthCollection, _receiveTruthOf } from "./_downstreamOps";
-import { _requestMediaContents, _prepareBvob } from "./_mediaOps";
+import { _chronicleEventLog, _narrateEventLog } from "./_eventLogOps";
+import { _connect } from "./_connectionOps";
 
 /**
  * The nexus connection object, which consolidates the local scribe connections and the possible
@@ -27,46 +27,10 @@ import { _requestMediaContents, _prepareBvob } from "./_mediaOps";
  * @extends {PartitionConnection}
  */
 export default class OraclePartitionConnection extends PartitionConnection {
-  _lastAuthorizedEventId: number;
-  _downstreamTruthQueue: Object[];
-  _retrieveMediaContentFromAuthority: ?RetrieveMediaContent;
-
-  constructor (options: Object) {
-    super(options);
-    this._lastAuthorizedEventId = -1;
-    this._downstreamTruthQueue = [];
-    this._isConnected = false;
+  constructor ({ authorityProphet, ...rest }: Object) {
+    super(rest);
+    this._authorityProphet = authorityProphet;
   }
-
-  getScribeConnection (): PartitionConnection {
-    return this._upstreamConnection;
-  }
-
-  getRetrieveMediaContent () {
-    return this._retrieveMediaContentFromAuthority;
-  }
-
-  isConnected (): boolean {
-    return this._isConnected;
-  }
-
-  _getOwnPartitionInfoOf (event: Object) {
-    const partitionURIString = String(this.getPartitionURI());
-    let ret = event.partitions[partitionURIString];
-    if (!ret) {
-      ret = event.partitions[this.getPartitionRawId()] || event.partitions[""];
-      // const partitionAuthorityURI = (this._authorityConnection || {})
-      // if (this._authorityConnection) {
-      //  invariantifyString(partitionInfo.partitionAuthorityURI, "partitionInfo.partitionAuthorityURI",
-      //      { value: this.partitionAuthorityURI})
-      // }
-    }
-    invariantifyObject(ret,
-        `event.partitions["${partitionURIString}" || "${this.getPartitionRawId()}"]`, {},
-        "\n\tevent:", event);
-    return ret;
-  }
-
 
   /**
    * Asynchronous operation which activates the connection to the Scribe and loads its metadatas,
@@ -86,17 +50,11 @@ export default class OraclePartitionConnection extends PartitionConnection {
    * @memberof OraclePartitionConnection
    */
   async connect (options: ConnectOptions) {
-    const onConnectData = { ...options };
     try {
-      this.warnEvent(1, "\n\tBegun initializing connection with options", options,
-          ...dumpObject(this));
-      const ret = await _connect(this, onConnectData);
-      this.warnEvent(1, "\n\tDone initializing connection with options", options,
-          "\n\tinitial narration:", ret);
-      return ret;
+      return await _connect(this, options);
     } catch (error) {
       throw this.wrapErrorEvent(error, "connect",
-          "\n\tonConnectData:", onConnectData);
+          "\n\toptions:", ...dumpObject(options));
     }
   }
 
@@ -107,7 +65,8 @@ export default class OraclePartitionConnection extends PartitionConnection {
     } catch (error) {
       throw this.wrapErrorEvent(error, "narrateEventLog()",
           "\n\toptions:", ...dumpObject(options),
-          "\n\tcurrent ret:", ...dumpObject(ret));
+          "\n\tcurrent ret:", ...dumpObject(ret),
+      );
     }
   }
 
@@ -124,91 +83,61 @@ export default class OraclePartitionConnection extends PartitionConnection {
     }
   }
 
-  createReceiveTruth (originName: string) {
-    return this._receiveTruthOf.bind(this, { name: originName });
-  }
-
-  /**
-   * Creates a truth Event receiver collection on this connection for performant side effect
-   * grouping and returns it.
-   *
-   * The collection.receiveTruth will process individual truths and forwards them to Scribe like
-   * createReceiveTruth does.
-   *
-   * The collection postpones and groups complex operations with costly overheads together.
-   * Most notably media retrievals will be postponed to the collection finalize phase (which is
-   * triggered by calling collection.finalize). This allows dropping unneeded media retrievals and
-   * potentially grouping all the retrievals into a single multi-part request.
-   *
-   * Additionally later other types of side-effect groupings can be added, like indexeddb writes
-   * and persistence refcount updates: these are not yet implemented however and are done
-   * one-by-one.
-   *
-   * @param {string}   collectionName
-   * @param {Object}   { retrieveMediaContent, requestMediaContents }
-   * @returns {Object} { receiveTruth, finalize, retrieveMediaContent, analyzeRetrievals }
-   * @memberof OraclePartitionConnection
-   */
-  createReceiveTruthCollection (name: string,
-      { retrieveMediaContent = this.getRetrieveMediaContent(), requestMediaContents }: Object): {
-    receiveTruth: Function,
-    finalize: Function,
-    analyzeRetrievals: Function,
-  } {
-    return _createReceiveTruthCollection(this,
-        { name, retrieveMediaContent, requestMediaContents });
-  }
-
-  async _receiveTruthOf (group: Object, truthEvent: UniversalEvent): Promise<Object> {
-    let partitionInfo;
-    try {
-      partitionInfo = this._getOwnPartitionInfoOf(truthEvent);
-      return _receiveTruthOf(this, group, partitionInfo.eventId, truthEvent);
-    } catch (error) {
-      throw this.wrapErrorEvent(error, `_receiveTruthOf('${group.name}')`,
-          "\n\tgroup:", ...dumpObject(group),
-          "\n\tpartitionInfo:", ...dumpObject(partitionInfo),
-          "\n\ttruthEvent:", ...dumpObject(truthEvent),
-          "\n\tthis:", ...dumpObject(this));
-    }
-  }
-
-  _preAuthorizeCommand = this.createReceiveTruth("preAuthorizer")
-
   // Coming from downstream: tries scribe first, otherwise forwards the request to authority.
   // In latter case forwards the result received from authority to Scribe for caching.
   requestMediaContents (mediaInfos: MediaInfo[]): any[] {
-    return mediaInfos.map(mediaInfo => {
-      let ret;
-      let combinedInfo;
+    const upstreamURLRequests = {};
+    const upstreamContentRequests = {};
+    const ret = mediaInfos.map(mediaInfo => {
       try {
-        combinedInfo = {
-          ...this.getScribeConnection().getMediaInfo(mediaInfo.mediaId),
-          ...mediaInfo,
-        };
-        return _requestMediaContents(this, [combinedInfo], onError.bind(this))[0];
-      } catch (error) { throw onError.call(this, error); }
-      // Store the content to Scribe as well (but not to authority): dont wait for completion
-      function onError (error) {
-        return this.wrapErrorEvent(error, `requestMediaContents(${
-                (combinedInfo || mediaInfo).name || `unnamed media`})`,
-            "\n\tmediaId:", mediaInfo.mediaId,
-            "\n\tcombined MediaInfo:", combinedInfo,
-            "\n\tresult candidate:", ret);
+        if (!mediaInfo.bvobId) {
+          if (!(mediaInfo.asURL && mediaInfo.sourceURL)) return undefined;
+          const sourceURI = createValaaURI(mediaInfo.sourceURL);
+          if (mediaInfo.asURL) {
+            if (sourceURI.protocol === "http:" || sourceURI.protocol === "https:") {
+              return mediaInfo.sourceURL;
+            }
+            // TODO(iridian): Implement schema-based request forwarding to remote authorities
+            throw new Error(`non-http(s) mediaInfo.sourceURL's not implemented, got '${
+                sourceURI.toString()}'`);
+          }
+          // TODO(iridian): Implement schema-based request forwarding to authorities
+          // TODO(iridian): Implement straight mediaInfo.sourceURL retrieval if the field is
+          // present, using mediaInfo.type/subtype as the request ContentType.
+          throw new Error(`direct retrieval not implemented for mediaInfo.sourceURL '${
+              sourceURI.toString()}'`);
+        }
+        // Split requests into two sets: one for URL's, one for actual contents.
+        return _addDelayedEntry(mediaInfo,
+            mediaInfo.asURL ? upstreamURLRequests : upstreamContentRequests);
+      } catch (error) {
+        return this.wrapErrorEvent(error, `requestMediaContents().mediaInfo["${
+          mediaInfo.name || `unnamed media`}"]`,
+      "\n\tmediaId:", mediaInfo.mediaId,
+      "\n\tmediaInfo:", ...dumpObject(mediaInfo));
       }
     });
-  }
-
-  // Coming from downstream: stores Media content in Scribe and uploads it to possible remote
-  // uploads pool.
-  prepareBvob (content: any, mediaInfo: Object, options: any = {}):
-      { contentId: string, persistProcess: ?Promise<any> } {
-    try {
-      return _prepareBvob(this, content, mediaInfo, options);
-    } catch (error) {
-      throw this.wrapErrorEvent(error, `prepareBvob()`,
-          "\n\tmediaInfo:", ...dumpObject(mediaInfo),
-      );
+    if (upstreamURLRequests.entries) {
+      upstreamURLRequests.resolveWith(
+          this.getUpstreamConnection().requestMediaContents(upstreamURLRequests.entries));
     }
+    if (upstreamContentRequests.entries) {
+      upstreamContentRequests.resolveWith(
+          this.getUpstreamConnection().requestMediaContents(upstreamContentRequests.entries));
+    }
+    return ret;
   }
+}
+
+function _addDelayedEntry (operation, entry) {
+  if (!operation.entries) {
+    operation.entries = [];
+    operation.results = new Promise(
+        (resolve, reject) => { operation.resolve = resolve; operation.reject = reject; });
+    operation.resolveWith = maybeThenable =>
+        Promise.resolve(maybeThenable).then(operation.resolve, operation.reject);
+  }
+  const index = operation.entries.length;
+  operation.entries.push(entry);
+  return operation.results.then(values => values[index]);
 }

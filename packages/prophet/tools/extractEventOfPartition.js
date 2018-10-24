4ae2553ce71ae8769791d0991c132663eae41cb4
iridian <iridian@iki.fi>
2018-10-24 16:35:27 +0300

    refactor(prophet): Update event version to 0.2, add legacy upgradeEvent*
    Adds the upgradeEvent call sites to both InspireGateway revelation
    pathway and to OraclePC downstream event push pathway.

---
+++ packages/prophet/tools/extractEventOfPartition.js
@@ -0,0 +1,42 @@
+// @flow
+
+import { Action, isTransactedLike } from "~/raem/command";
+
+import PartitionConnection from "~/prophet/api/PartitionConnection";
+
+import { dumpObject } from "~/tools";
+
+export default function extractEventOfPartition (action: Action,
+    connection: PartitionConnection, partitionKey: string = String(connection.getPartitionURI())) {
+  let ret;
+  try {
+    if (!(action.partitions || {})[partitionKey]) return undefined;
+    ret = { ...action };
+    delete ret.partitions;
+    if (!ret.version) {
+      // TODO(iridian): Fix @valos/raem so that it doesn't generate commandId for sub-actions.
+      delete ret.commandId;
+    }
+    if (Object.keys(action.partitions).length !== 1) {
+      if (!isTransactedLike(action)) {
+        throw new Error("Non-TRANSACTED-like multipartition proclamations are not supported");
+      }
+      ret.actions = action.actions
+          .map(action => extractEventOfPartition(action, connection, partitionKey))
+          .filter(notFalsy => notFalsy);
+      if (!ret.actions.length) {
+        throw new Error(`INTERNAL ERROR: No TRANSACTED-like.actions found for current partition ${
+            ""}in a multi-partition TRANSACTED-like action`);
+      }
+    }
+    return ret;
+  } catch (error) {
+    throw connection.wrapErrorEvent(error,
+        new Error(`extractEventOfPartition(${connection.getName()})`),
+        "\n\tpartitionKey:", partitionKey,
+        "\n\tproclamation:", ...dumpObject(action),
+        "\n\tproclamation partitions:", ...dumpObject(action.partitions),
+        "\n\tcurrent ret:", ...dumpObject(ret),
+    );
+  }
+}
---
+++ packages/prophet/tools/upgradeEventToVersion0dot2.js
@@ -0,0 +1,30 @@
+// @flow
+
+import { UniversalEvent } from "~/raem/command";
+
+import PartitionConnection from "~/prophet/api/PartitionConnection";
+import { dumpObject } from "~/tools";
+
+import extractEventOfPartition from "./extractEventOfPartition";
+
+export default function upgradeEventToVersion0dot2 (event: UniversalEvent,
+    connection: PartitionConnection) {
+  try {
+    if (event.version === "0.2") return event;
+    let ret;
+    if (event.version === "0.1") {
+      ret = extractEventOfPartition(event, connection, connection.getPartitionRawId());
+    } else {
+      throw new Error(`Unrecognized event version "${event.version
+          }" when trying to upgrade event to version 0.2`);
+    }
+    // if (!ret) return ret;
+    ret.version = "0.2";
+    return ret;
+  } catch (error) {
+    throw connection.wrapErrorEvent(error, new Error("upgradeEventToVersion0dot2()"),
+      "\n\tevent:", ...dumpObject(event),
+      "\n\tevent.partitions:", ...dumpObject(event.partitions),
+      "\n\tconnection:", connection.getName());
+  }
+}

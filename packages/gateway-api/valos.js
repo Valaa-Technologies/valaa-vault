e2bb346c00f7b57880c6986cce2897239d443fed
iridian <iridian@iki.fi>
2019-04-30 11:11:39 +0300

    feature: Adds valosheath.identity session management base implementation

--- packages/gateway-api/valos.js
+++ packages/gateway-api/valos.js
@@ -1,4 +1,5 @@
 import getGlobal from "~/gateway-api/getGlobal";
+import { base64URLDecode } from "~/gateway-api/base64";
 
 const inBrowser = require("~/gateway-api/inBrowser").default;
 
@@ -57,4 +58,65 @@ export default (getGlobal().valos ||
     return plugin;
   },
   plugins: [],
+
+  identity: {
+    getSessionClaims (options = {}) {
+      const sessionCookieName = this.getClientCookieName(options);
+      for (const line of window.document.cookie.split(";")) {
+        const [name, value] = line.split("=");
+        if (name.trim() === sessionCookieName) {
+          const payload = value.trim().split(".")[1];
+          const claims = payload && JSON.parse(base64URLDecode(payload));
+          return claims;
+        }
+      }
+      return undefined;
+    },
+
+    getClientCookieName (options) {
+      if (!options.client_id) options.client_id = options.clientURI || this._clientURI;
+      if (!options.client_id) throw new Error("getClientCookieName.(clientURI|client_id) missing");
+      return `__Secure-valos-client-${encodeURIComponent(options.client_id)}`;
+    },
+
+    authorizeSession ({ clientURI, grantProvider, sessionURI, ...rest }) {
+      if (!inBrowser()) throw new Error("Cannot authorize a session in non-browser context");
+      if (!grantProvider) throw new Error("authorizeSession.grantProvider missing");
+      if (grantProvider.substr(0, 8) !== "https://") {
+        throw new Error("Invalid grant provider: only OpenId Connect https:// scheme is supported");
+      }
+      const params = { client_id: clientURI, ...rest };
+      if (sessionURI) params.redirect_uri = sessionURI;
+      if (!params.redirect_uri) {
+        throw new Error("authorizeSession.(sessionURI|redirect_uri) missing");
+      }
+      const clientCookieName = this.getClientCookieName(params);
+      if (params.scope === undefined) {
+        params.scope = "openid profile";
+      }
+      if (params.state === undefined) {
+        params.state = (`${Math.random().toString(36)}000000000`).slice(2, 7);
+      }
+      if (params.state) {
+        window.document.cookie = `${clientCookieName}=${encodeURIComponent(params.state)
+          }; max-age=900; Secure; SameSite=Lax`;
+      }
+      let redirector = `${grantProvider}?response_type=code`;
+      Object.keys(params).forEach(k => {
+        if (params[k]) redirector += `&${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`;
+      });
+      window.location.replace(redirector);
+    },
+
+    finalizeSession (options = {}) {
+      if (!inBrowser()) throw new Error("Cannot finalize a session in non-browser context");
+      if (!options.sessionURI) throw new Error("finalizeSession.sessionURI missing");
+      window.document.cookie = `${this.getClientCookieName(options)}=;max-age=0`;
+      window.document.cookie = `__Secure-valos-session-token-${
+        encodeURIComponent(options.client_id)}=;max-age=0`;
+      window.fetch(options.sessionURI, {
+        method: "DELETE", credentials: "same-origin", mode: "same-origin", redirect: "error",
+      });
+    }
+  },
 }));

069a9b48eb36636d11475edeb444f803db77a704
Jaradacl <rassse333@gmail.com>
2019-07-12 17:55:35 +0300

    feat: Add query-authority lib
    docs: Add 3 new tutorials

---
+++ packages/query-authority/babel.config.js
@@ -0,0 +1,5 @@
+// This file exists so that vlm assemble-packages triggers babel
+      // transpilation for this module.
+      // Options here are merged on top of root babel.config.js
+      module.exports = {};
+      

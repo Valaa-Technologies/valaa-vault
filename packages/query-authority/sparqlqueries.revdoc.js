aa591a39ff15a457d87754b4df6739418410306d
Jaradacl <rassse333@gmail.com>
2019-07-22 12:36:47 +0300

    docs: Add SPARQL queries revdoc
    Fix AssortedTutorials
    
    feat: Add CorpusQuadSource class

---
+++ packages/query-authority/sparqlqueries.revdoc.js
@@ -0,0 +1,34 @@
+
+const {
+  extractee: { cdata, authors, ref, context, cli, command, cpath, bulleted, pkg },
+} = require("@valos/toolset-vault/revdoc");
+
+module.exports = {
+  "dc:title": "SPARQL Queries for PoT/TTP hypertwin",
+  respecConfig: {
+    specStatus: "unofficial",
+    editors: authors("iridianI"),
+    authors: authors("iridian", "Jaradacl"),
+    shortName: "sparqlqueries",
+  },
+  "chapter#abstract>0": [
+    "This document is a revdoc template document 'sparqlqueries' created by create-revdoc.",
+  ],
+  "chapter#sotd>1": [
+    "This document is part of the vault workspace ",
+    ref("@valos/kernel_"),
+    " (of domain ", ref("@valos/kernel"), ") which is ",
+    "ValOS common infrastructure tools and libraries monorepo.",
+  ],
+  "chapter#introduction>2": [
+    "Edit me - this is the first payload chapter. Abstract and SOTD are essential",
+    ref("ReSpec boilerplate", "https://github.com/w3c/respec/wiki/ReSpec-Editor's-Guide#essential-w3c-boilerplate"),
+    null,
+    "See ", ref("ReVDoc tutorial", "@valos/toolset-vault/revdoc/tutorial"),
+    " for instructions on how to write revdoc source documents.",
+    null,
+    "See also ", ref("ReVdoc specification", "@valos/toolset-vault/revdoc"),
+    " and ", ref("VDoc specification", "@valos/toolset-vault/vdoc"),
+    " for reference documentation.",
+  ],
+};

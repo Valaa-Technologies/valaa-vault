b600f10f393ff1656d7dc034e451842d51eeeebb
Jaradacl <rassse333@gmail.com>
2019-09-10 15:31:49 +0300

    refactor(query-authority): Remove PoT specific files
    refactor(query-authority): Remove babel.config.js
    
    refactor(query-authority): Remove deprecated or obsolete files

--- packages/query-authority/babel.config.js
+++
@@ -1,5 +0,0 @@
-// This file exists so that vlm assemble-packages triggers babel
-      // transpilation for this module.
-      // Options here are merged on top of root babel.config.js
-      module.exports = {};
-      
